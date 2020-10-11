46173ebb8cbbe71526423fdb116b6b3ce0f6aacb
greatcoder333 <71765346+greatcoder333@users.noreply.github.com>
2020-10-11 15:30:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var rectangle1
+
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	World.add(world, packageBody);
+	
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	Engine.run(engine);
+
+rectangle1 = new Rectangles(300,300,50,50)
+rectangle2 = new Rectangles(100,200,50,50)
+rectangle3 = new Rectangles(50,25,50,50)
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+rectangle1.display()
+rectangle2.display()
+rectangle3.display()
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody,false)
+  }
+if (keyCode === LEFT_ARROW){
+helicopterSprite.x = helicopterSprite.x -20
+translation = {x:-20 , y:0}
+Matter.Body.translate (packageBody,translation)
+}
+else if (keyCode === RIGHT_ARROW){
+helicopterSprite.x = helicopterSprite.y - 30
+Matter.Body.translate(packageBody,translation)
+}
+if (packageBody.pos.x = packageBody.pos.y){
+packageBody.velocity(0,0)
+}
+}
+
+
+
